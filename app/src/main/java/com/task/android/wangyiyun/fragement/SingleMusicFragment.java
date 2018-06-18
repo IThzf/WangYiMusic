bf62331f79173948b5ed6a7e71643b6fcbff103d
MyGit <2911106313@qq.com>
2018-06-18 20:33:45 +0800

    第三次合并

--- app/src/main/java/com/task/android/wangyiyun/fragement/SingleMusicFragment.java
+++ app/src/main/java/com/task/android/wangyiyun/fragement/SingleMusicFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.task.android.wangyiyun.R;
+import com.task.android.wangyiyun.activity.PlayMusicActivity;
 import com.task.android.wangyiyun.adapter.MusicListAdapter;
 
 import java.util.ArrayList;
